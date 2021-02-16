2ba0c2ac083483bbe9fb9a490283dd65bf628981
amandodaw <47083109+amandodaw@users.noreply.github.com>
2021-02-16 02:59:43 +0100

    Sí

--- ZombieArcadeProto/NPC/Zombie.gd
+++ ZombieArcadeProto/NPC/Zombie.gd
@@ -2,16 +2,27 @@ extends KinematicBody2D
 
 onready var ray2d: RayCast2D = $RayCast2D
 
-enum state_enum { Idle, Walking }
+enum state_enum { Idle, Walking, Chasing }
 
-var speed = 100
+const TYPE = "ZOMBIE"
+
+var health = 50 setget damage_taken
+var speed = 50
+
 var direction = Vector2.ZERO
+var objective: Node
 var spritedir = "Down"
 var state = state_enum.Idle setget change_state
 
 
 # Called when the node enters the scene tree for the first time.
 func _physics_process(delta):
+	match state:
+		state_enum.Idle, state_enum.Walking:
+			pass
+		state_enum.Chasing:
+			direction = (objective.position - position)
+			move_and_slide(direction.normalized()*speed)
 	anim_dir()
 
 
@@ -25,9 +36,29 @@ func anim_dir():
 			spritedir = "Down"
 		Vector2.UP:
 			spritedir = "Up"
-	$anim.play(str(state_enum.keys()[state], spritedir))
-
+	if state_enum.keys()[state] == "Chasing":
+		$anim.play(str("Walking", spritedir))
+	else:
+		$anim.play(str(state_enum.keys()[state], spritedir))
 
 func change_state(value: int):
 	state = value
 	print("Nuevo estado: ", state_enum.keys()[state])
+
+
+
+func damage_taken(value):
+	health = value
+	if health <= 0:
+		queue_free()
+
+
+func _on_vision_body_entered(body):
+	if body.TYPE == "PLAYER":
+		objective = body
+		change_state(state_enum.Chasing)
+
+
+func _on_vision_body_exited(body):
+		if body == objective:
+			change_state(state_enum.Idle)
--- ZombieArcadeProto/Player/Player.gd
+++ ZombieArcadeProto/Player/Player.gd
@@ -1,10 +1,13 @@
 extends KinematicBody2D
 
 onready var gun_scene = load('res://Objects/Gun.tscn')
+var gun: Sprite
 
 onready var ray2d: RayCast2D = $RayCast2D
 
 enum state_enum { Idle, Walking, Aiming }
+
+const TYPE = "PLAYER"
 
 var speed = 100
 var direction = Vector2.ZERO
@@ -12,16 +15,23 @@ var spritedir = "Down"
 var state = state_enum.Idle setget change_state
 
 
+signal change_aim
+signal stop_aim
 #Variables que serán cambiadas porque son propiedades que pertenecen a nodos que aún no existen
 var gun_alcance = 16 * 10
 
 
 func _input(event):
 	if event.is_action_pressed("disparar"):
-		change_state(state_enum.Aiming)
+		aim()
 	elif event.is_action_released("disparar"):
 		ray2d.cast_to = Vector2.ZERO
 		change_state(state_enum.Idle)
+		emit_signal("stop_aim")
+		if ray2d.is_colliding():
+			var collider = ray2d.get_collider()
+			if collider.TYPE == "ZOMBIE":
+				collider.health = collider.health - gun.damage
 
 
 # Called when the node enters the scene tree for the first time.
@@ -30,7 +40,7 @@ func _physics_process(delta):
 		state_enum.Idle, state_enum.Walking:
 			move_control()
 		state_enum.Aiming:
-			aim_control()
+			raycast_control()
 
 func control():
 	direction.x = Input.get_action_strength("derecha") - Input.get_action_strength("izquierda")
@@ -48,10 +58,16 @@ func move_control():
 	move_and_slide(direction.normalized() * speed)
 
 
-func aim_control():
-	var gun = gun_scene.instance()
+func aim():
+	change_state(state_enum.Aiming)
+	gun = gun_scene.instance()
+	raycast_control()
 	add_child(gun)
-	gun.position = direction
+	connect("change_aim", gun, "change_aim")
+	connect("stop_aim", gun, "die")
+
+
+func raycast_control():
 	control()
 	if direction == Vector2.ZERO:
 		match spritedir:
@@ -63,10 +79,9 @@ func aim_control():
 				ray2d.cast_to = Vector2.UP * gun_alcance
 			"Down":
 				ray2d.cast_to = Vector2.DOWN * gun_alcance
-		return
-	ray2d.cast_to = direction.normalized()*gun_alcance
-
-	
+	else:
+		ray2d.cast_to = direction.normalized()*gun_alcance
+	emit_signal("change_aim", direction, spritedir)
 
 func anim_dir():
 	match direction:
